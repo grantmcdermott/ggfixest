4ab8d7f9c0a29b1973fce6f74ccb82a9fea45c89
Grant McDermott <gmcd@amazon.com>
2023-12-13 13:13:49 -0800

    Also control magick nthreads
    - OpenMP everywhere...

--- inst/tinytest/test_nthreads.R
+++ inst/tinytest/test_nthreads.R
@@ -1,5 +1,7 @@
 library(tinytest)
 
+exit_if_not(any(grepl("_R_CHECK", names(Sys.getenv()), fixed = TRUE)))
+
 # fixest
 if ( requireNamespace("fixest", quietly=TRUE) ){
 	library(fixest)
@@ -8,8 +10,15 @@ if ( requireNamespace("fixest", quietly=TRUE) ){
 }
 
 # data.table
-if(requireNamespace("data.table", quietly = TRUE)){
-	library(data.table)
-	nDT = getDTthreads()
-	expect_equal(nDT, 1, info = "Check data.table threads")
+if (requireNamespace("data.table", quietly = TRUE)) {
+    library(data.table)
+    nDT = getDTthreads()
+    expect_equal(nDT, 1, info = "Check data.table threads")
+}
+
+# magick
+if (requireNamespace("magick", quietly = TRUE)) {
+    library(magick)
+    nmg = magick:::magick_threads(1)
+    expect_equal(nmg, 1, info = "Check magick threads")
 }
--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,15 +1,23 @@
 ## Throttle CPU threads if R CMD check (for CRAN)
 
-# fixest
-if ( requireNamespace("fixest", quietly=TRUE) ){
-	library(fixest)
-	setFixest_nthreads(1)
-}
+if (any(grepl("_R_CHECK", names(Sys.getenv()), fixed = TRUE))) {
+    # fixest
+    if (requireNamespace("fixest", quietly = TRUE)) {
+        library(fixest)
+        setFixest_nthreads(1)
+    }
 
-# data.table
-if(requireNamespace("data.table", quietly = TRUE)){
-	library(data.table)
-	setDTthreads(1)
+    # data.table
+    if (requireNamespace("data.table", quietly = TRUE)) {
+        library(data.table)
+        setDTthreads(1)
+    }
+
+    # magick
+    if (requireNamespace("magick", quietly = TRUE)) {
+        library(magick)
+        magick:::magick_threads(1)
+    }
 }
 
 
